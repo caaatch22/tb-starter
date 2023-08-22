1251e83ec118381d08320fa63e100f3d20a0d783
caaatch22 <mr.liumingjie@gmail.com>
2023-08-22 14:23:42 +0800

    feat: add more benchmarks for parallel_hash_map_benchmark

--- single/parallel_mp.cpp
+++ single/parallel_mp.cpp
@@ -32,16 +32,22 @@ using RW = T*;
 template <typename K, typename V>
 using fmp = phmap::flat_hash_map<K, V>;
 
-using Data = tbs::MockData;
+template <typename K, typename V>
+using nodemp = phmap::node_hash_map<K, V>;
 
-TEST_CASE("benchmark", "[inserting 1000000 integer]") {
-  cout << "ph-hashtable and std-hashtable benchmark, inserting 1000000 "
-          "integer:\n";
+using Data = tbs::MockData;
+using tbs::BigMockData;
+
+TEST_CASE("benchmark integer", "[inserting 1000000 integers]") {
+  cout << "ph-hashtable and std-hashtable benchmark" << '\n';
+  cout << "inserting 1000000 integers:\n";
 
   Timer timer;
 
   std::unordered_map<int, int> stdmp;
+  nodemp<int, int> nodemp;
   fmp<int, int> mp;
+
   constexpr int n = 1e6;
   auto const keys = tbs::distinct_vec<int, 0>(n);
 
@@ -49,25 +55,110 @@ TEST_CASE("benchmark", "[inserting 1000000 integer]") {
   for (auto const k : keys) {
     mp[k] = rng();
   }
-  cout << "ph-hashtable: " << timer << '\n';
+  cout << "flat-hashtable: " << timer << '\n';
 
   timer.reset();
   for (auto const k : keys) {
     stdmp[k] = rng();
   }
   cout << "std-hahtable: " << timer << '\n';
+
+  timer.reset();
+  for (auto const k : keys) {
+    nodemp[k] = rng();
+  }
+  cout << "node-hahtable: " << timer << '\n';
+
+  cout << "iterate over 1000000 integers:" << '\n';
+  timer.reset();
+  int sum = 0;
+  for (auto const& [_, v] : mp) {
+    sum += v;
+  }
+  cout << "flat-hashtable time: " << timer << ", sum = " << sum << '\n';
+
+  timer.reset();
+  for (auto const& [_, v] : stdmp) {
+    sum += v;
+  }
+  cout << "std-hashtable time: " << timer << ", sum = " << sum << '\n';
+
+  timer.reset();
+  for (auto const& [_, v] : nodemp) {
+    sum += v;
+  }
+  cout << "node-hashtable time: " << timer << ", sum = " << sum << '\n';
   cout << '\n';
 }
 
-TEST_CASE("benchmark of phmap with key type string") {
-  cout << "ph-hashtable and std-hashtable benchmark, inserting 1000000 "
-          "string(sso):\n";
+TEST_CASE("flat map reserve", "[inserting 1000000 integers]") {
+  cout << "inserting 1000000 integers with reserve benchmark" << '\n';
+
+  Timer timer;
+
+  std::unordered_map<int, int> stdmp;
+  fmp<int, int> mp;
+  nodemp<int, int> nodemp;
+
+  stdmp.reserve(2e6);
+  mp.reserve(2e6);
+  nodemp.reserve(2e6);
+
+  constexpr int n = 1e6;
+  auto const keys = tbs::distinct_vec<int, 0>(n);
+
+  timer.reset();
+  for (auto const k : keys) {
+    mp[k] = rng();
+  }
+  cout << "flat-hashtable with reserve: " << timer << '\n';
+
+  timer.reset();
+  for (auto const k : keys) {
+    stdmp[k] = rng();
+  }
+  cout << "std-hahtable with reserve: " << timer << '\n';
+
+  timer.reset();
+  for (auto const k : keys) {
+    nodemp[k] = rng();
+  }
+  cout << "node-hashtable with reserve: " << timer << '\n';
+
+  cout << "iterate over 1000000 integers:" << '\n';
+  timer.reset();
+  int sum = 0;
+  for (auto const& [_, v] : mp) {
+    sum += v;
+  }
+  cout << "flat-hashtable time: " << timer << ", sum = " << sum << '\n';
+
+  timer.reset();
+  for (auto const& [_, v] : stdmp) {
+    sum += v;
+  }
+  cout << "std-hashtable time: " << timer << ", sum = " << sum << '\n';
+
+  timer.reset();
+  for (auto const& [_, v] : nodemp) {
+    sum += v;
+  }
+  cout << "node-hashtable time: " << timer << ", sum = " << sum << '\n';
+  cout << '\n';
+}
+
+TEST_CASE("benchmark string", "[inserting 1000000 strings]") {
+  cout << "ph-hashtable and std-hashtable benchmark" << '\n';
+  cout << "inserting 1000000 string(sso):\n";
+
   Timer timer;
 
   std::unordered_map<string, UP<Data>> stdmp;
   fmp<string, UP<Data>> mp;
+  nodemp<string, UP<Data>> nodemp;
+
   constexpr int n = 1e6;
-  auto const keys = tbs::rng_dates(n, "20220120"sv, "90900530"sv);
+  auto const keys = tbs::rng_dates(n, "10220120"sv, "90900530"sv);
 
   auto values1 = vector<RW<Data>>(n);
   for (int i = 0; i < n; i++) {
@@ -79,17 +170,82 @@ TEST_CASE("benchmark of phmap with key type string") {
     values2[i] = new Data;
   };
 
+  auto values3 = vector<RW<Data>>(n);
+  for (int i = 0; i < n; i++) {
+    values3[i] = new Data;
+  };
+
   timer.reset();
   for (int i = 0; i < n; i++) {
     mp[keys[i]] = unique_ptr<Data>{values1[i]};
   }
-  cout << "ph-hashtable: " << timer << '\n';
+  cout << "flat-hashtable: " << timer << '\n';
 
   timer.reset();
   for (int i = 0; i < n; i++) {
     stdmp[keys[i]] = unique_ptr<Data>{values2[i]};
   }
   cout << "std-hashtable: " << timer << '\n';
+
+  timer.reset();
+  for (int i = 0; i < n; i++) {
+    nodemp[keys[i]] = unique_ptr<Data>{values3[i]};
+  }
+  cout << "node-hashtable: " << timer << '\n';
+
+  cout << "iterate over 1000000 strings:" << '\n';
+  timer.reset();
+  Data sum;
+  for (auto const& [_, v] : mp) {
+    sum.eamcode += v->eamcode;
+  }
+  cout << "flat-hashtable time: " << timer << ", sum = " << sum.eamcode << '\n';
+
+  timer.reset();
+  for (auto const& [_, v] : stdmp) {
+    sum.eamcode += v->eamcode;
+  }
+  cout << "std-hashtable time: " << timer << ", sum = " << sum.eamcode << '\n';
+
+  timer.reset();
+  for (auto const& [_, v] : nodemp) {
+    sum.eamcode += v->eamcode;
+  }
+  cout << "node-hashtable time: " << timer << ", sum = " << sum.eamcode << '\n';
+  cout << '\n';
+}
+
+TEST_CASE("benchmark with big value") {
+  cout << "inserting 1000000 integers as key and big data as value:\n";
+
+  Timer timer;
+
+  std::unordered_map<int64_t, BigMockData> stdmp;
+  fmp<int64_t, BigMockData> mp;
+  nodemp<int64_t, BigMockData> nodemp;
+
+  const int n = 1e6;
+  auto const keys = tbs::distinct_vec<int, 0>(n);
+
+  timer.reset();
+  for (auto const k : keys) {
+    mp[k] = BigMockData();
+  }
+  cout << "flat-hashtable: " << timer << '\n';
+
+  timer.reset();
+  for (auto const k : keys) {
+    stdmp[k] = BigMockData();
+  }
+  cout << "std-hashtable: " << timer << '\n';
+
+  timer.reset();
+  for (auto const k : keys) {
+    nodemp[k] = BigMockData();
+  }
+  cout << "node-hashtable: " << timer << '\n';
+
+  cout << '\n';
 }
 
 TEST_CASE("validate in rehash of phmap") {
--- tbs/include/utils.hpp
+++ tbs/include/utils.hpp
@@ -55,6 +55,44 @@ struct MockData {
       : symbol(std::move(smb)), eamcode(code) {}
 };
 
+struct BigMockData {
+  int64_t epoch_local_time = 0;
+  std::string symbol{};
+  std::string market{};
+  uint32_t stream_id = 0;
+  int64_t epoch_exchg_time = 0;
+  uint16_t channel = 0;
+  int32_t status = 0;
+  double pre_close = 0.0;
+  double open = 0.0;
+  double high = 0.0;
+  double low = 0.0;
+  double match = 0.0;
+  double close = 0.0;
+  std::vector<double> ask_px{};
+  std::vector<double> bid_px{};
+  std::vector<int64_t> ask_vol{};
+  std::vector<int64_t> bid_vol{};
+  double upper_limit = 0.0;
+  double lower_limit = 0.0;
+  uint64_t volume = 0;
+  double amount = 0.0;
+  double settle_px = 0.0;
+  double pre_settle_px = 0.0;
+  int64_t interest = 0;
+  int64_t pre_interest = 0;
+  int32_t num_trades = 0;
+  int64_t total_bid_vol = 0;
+  int64_t total_ask_vol = 0;
+  double weighted_avg_bid_px = 0.0;
+  double weighted_avg_ask_px = 0.0;
+  uint32_t local_time = 0;
+  uint32_t exchg_time = 0;
+  int64_t trade_date = 0;
+  int32_t key = -1;
+  int64_t eam_code = 0;
+};
+
 struct NonCopy {
   std::string symbol{"000001.SZ"};
   int64_t eamcode{13000000000020020LL};
