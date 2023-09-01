e091cb0e4f41d33a34ffe5c8046627c831fd9ca2
caaatch22 <mr.liumingjie@gmail.com>
2023-09-01 10:34:24 +0800

    WIP: thread_safe queue and ringbuffer with bug

---
+++ single/thread_safe_queue.cpp
@@ -0,0 +1,46 @@
+#include "../tbs/include/thread_safe_queue.hpp"
+#include <BS_thread_pool.hpp>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "../tbs/include/lockfree_ring_buffer.hpp"
+#include "../tbs/include/random.hpp"
+#include "../tbs/include/timer.hpp"
+
+using std::string;
+using std::vector;
+using tbs::Timer;
+
+void validTest() {
+  constexpr int n = 100;
+  Timer<tbs::micro> timer;
+  ThreadSafeQueue<string> q;
+  BS::thread_pool pool(8);
+  vector<string> dates = tbs::rng_dates(n, "20220104", "49870107");
+
+  timer.reset();
+  pool.push_loop(dates.size(), [&](size_t l, size_t r) {
+    for (size_t i = l; i < r; i++) {
+      q.push(dates[i]);
+    }
+  });
+  pool.wait_for_tasks();
+  std::cout << timer << '\n';
+
+  LockFreeRingBuffer<string> rb;
+  timer.reset();
+  pool.push_loop(dates.size(), [&](size_t l, size_t r) {
+    for (size_t i = l; i < r; i++) {
+      rb.push(dates[i]);
+    }
+  });
+  pool.wait_for_tasks();
+
+  std::cout << timer << '\n';
+}
+
+int main() {
+  validTest();
+  return 0;
+}
---
+++ tbs/include/lockfree_ring_buffer.hpp
@@ -0,0 +1,78 @@
+#ifndef SRC_LOCKFREERINGBUFFER_HPP
+#define SRC_LOCKFREERINGBUFFER_HPP
+#include <atomic>
+#include <ranges>
+#include <vector>
+
+template <typename T>
+class LockFreeRingBuffer {
+ public:
+  LockFreeRingBuffer() : data_(kInitialSize) {
+    head_.store(0, std::memory_order_relaxed);
+    tail_.store(0, std::memory_order_relaxed);
+  }
+  explicit LockFreeRingBuffer(size_t capacity) : data_(capacity) {
+    head_.store(0, std::memory_order_relaxed);
+    tail_.store(0, std::memory_order_relaxed);
+  }
+
+  void push(const T& value) {
+    size_t tail = tail_.load(std::memory_order_relaxed);
+    size_t next_tail = next(tail);
+    if (next_tail == head_.load(std::memory_order_acquire)) {
+      expand();
+      next_tail = next(tail);
+    }
+    data_[tail] = value;
+    tail_.store(next_tail, std::memory_order_release);
+  }
+
+  bool pop(T& value) {
+    size_t head = head_.load(std::memory_order_relaxed);
+    if (head == tail_.load(std::memory_order_acquire)) {
+      return false;
+    }
+    value = std::move(data_[head]);
+    head_.store(next(head), std::memory_order_release);
+    return true;
+  }
+
+  bool empty() const {
+    return head_.load(std::memory_order_acquire) ==
+           tail_.load(std::memory_order_acquire);
+  }
+
+  size_t size() const {
+    size_t head = head_.load(std::memory_order_acquire);
+    size_t tail = tail_.load(std::memory_order_acquire);
+    return tail >= head ? tail - head : head - tail;
+  }
+
+ private:
+  static const size_t kInitialSize = 16;
+
+  size_t next(size_t current) const { return (current + 1) % data_.size(); }
+
+  void expand() {
+    std::vector<T> next_data(data_.size() * 2);
+    size_t head = head_.load(std::memory_order_acquire);
+    size_t tail = tail_.load(std::memory_order_acquire);
+    if (tail >= head) {
+      std::ranges::move(data_.begin() + head, data_.begin() + tail,
+                        next_data.begin());
+    } else {
+      std::ranges::move(data_.begin() + head, data_.end(), next_data.begin());
+      std::ranges::move(data_.begin(), data_.begin() + tail,
+                        next_data.begin() + data_.size() - head);
+    }
+    head_.store(0, std::memory_order_relaxed);
+    tail_.store(data_.size(), std::memory_order_relaxed);
+    data_ = std::move(next_data);
+  }
+
+  std::atomic<size_t> head_;
+  std::atomic<size_t> tail_;
+  std::vector<T> data_;
+};
+
+#endif /* SRC_LOCKFREERINGBUFFER_HPP */
